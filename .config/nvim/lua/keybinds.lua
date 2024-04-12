c500a0267128884238be9741cb0d8ac8fa1236e1
Philipp Alber <philipp_j.alber@yahoo.com>
2024-04-12 12:58:36 +0200

    add tmux configuration and integrate with vim navigation via vim-tmux-navigator

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -5,7 +5,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle<CR>")
 
 -- navigating panes easier than ctrl w and hjkl
-vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
-vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
-vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
-vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
+--vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
+--vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
+--vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
+--vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
