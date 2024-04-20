ae6d18ecb1ed1e32cfc9fa0c07e84ff223390141
Philipp Alber <philipp_j.alber@yahoo.com>
2024-04-20 18:09:27 +0200

    add rename keymap and some new options

--- .config/nvim/lua/set.lua
+++ .config/nvim/lua/set.lua
@@ -6,9 +6,14 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+vim.opt.autoindent = true
 vim.opt.wrap = true
 
 vim.opt.incsearch = true
+vim.opt.hlsearch = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.opt.scrolloff = 8
 
