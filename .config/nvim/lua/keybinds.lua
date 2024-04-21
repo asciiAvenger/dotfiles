da494768324c2b066b41d2b1c6841d29d36b9198
Philipp Alber <philipp_j.alber@yahoo.com>
2024-04-21 23:18:55 +0200

    add some new general and telescope keybindings

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -9,3 +9,11 @@ vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle<CR>")
 --vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
 --vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
 --vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
+
+-- quickfix list bindings
+vim.keymap.set("n", "<leader>qo", ":copen<CR>")
+vim.keymap.set("n", "<leader>qq", ":cclose<CR>")
+vim.keymap.set("n", "<leader>qn", ":cnext<CR>")
+vim.keymap.set("n", "<leader>qp", ":cprev<CR>")
+vim.keymap.set("n", "<leader>qf", ":cfirst<CR>")
+vim.keymap.set("n", "<leader>ql", ":clast<CR>")
