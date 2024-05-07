88b8660bd6d49783420aecf4d48696b50018aedf
Philipp Alber <philipp_j.alber@yahoo.com>
2024-05-07 16:23:13 +0200

    updated proton killscript

--- .bin/proton_killall.sh
+++ .bin/proton_killall.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-pkill -9 -f proton
+PIDS_TO_KILL=$(ps -ef | grep -E '[pP]roton|wine' | grep -v -E 'grep|proton_killall\.sh' | awk '{print $2}')
+
+for p in $PIDS_TO_KILL; do
+  kill -9 $p
+done
