be63ec2a3336dfa442309d77d5b70000b0737f79
Philipp Alber <philipp_j.alber@yahoo.com>
2024-05-08 17:05:49 +0200

    updated proton_killall script

--- .bin/proton_killall.sh
+++ .bin/proton_killall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PIDS_TO_KILL=$(ps -ef | grep -E '[pP]roton|wine' | grep -v -E 'grep|proton_killall\.sh' | awk '{print $2}')
+PIDS_TO_KILL=$(ps -ef | grep -E '[pP]roton|wine|windows' | grep -v -E 'grep|proton_killall\.sh' | awk '{print $2}')
 
 for p in $PIDS_TO_KILL; do
   kill -9 $p
