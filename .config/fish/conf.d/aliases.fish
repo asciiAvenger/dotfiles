b2bd325b3e1f247a09b8a3692b7e5b901d3281da
Philipp Alber <philipp_j.alber@yahoo.com>
2025-01-18 19:52:54 +0100

    fixed alias

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -4,7 +4,7 @@ alias yayss='yay -Ss'
 alias vim='nvim'
 alias vimdiff='nvim -d'
 alias ls='eza --color=auto --group-directories-first --icons'
-alias ll='eza --color=auto --group-directories-first --icons'
+alias ll='eza --color=auto --group-directories-first --icons -al'
 alias lt='eza --color=auto --group-directories-first --icons -aT'
 alias grep='grep --color=auto'
 
